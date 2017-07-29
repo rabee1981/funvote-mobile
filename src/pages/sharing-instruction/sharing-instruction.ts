3e8ef2d0121568242132f079e2f03417dfd2f920
mohamadrabee <mohamad_rabee10@yahoo.com>
2017-07-29 05:52:08 +0300

    design share instruction

--- src/pages/sharing-instruction/sharing-instruction.ts
+++ src/pages/sharing-instruction/sharing-instruction.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-sharing-instruction',
   templateUrl: 'sharing-instruction.html',
 })
 export class SharingInstructionPage {
-
-  constructor(private viewCtrl : ViewController, public navParams: NavParams) {
+  constructor(private viewCtrl : ViewController, public navParams: NavParams, private platform : Platform) {
   }
   onOk(){
     this.viewCtrl.dismiss();
