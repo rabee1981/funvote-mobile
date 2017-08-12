2ed1ba0026c994735b8d255ad2b5ac54674cc13c
mohamadrabee <mohamad_rabee10@yahoo.com>
2017-08-12 20:44:12 +0300

    wait 3 second in single chart when it first run

--- src/pages/single-chart/single-chart.ts
+++ src/pages/single-chart/single-chart.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, AlertController, AlertOptions } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @IonicPage({
   name: 'single-chart',
@@ -22,61 +23,72 @@ export class SingleChartPage implements OnInit, OnDestroy {
   chartSubscription: Subscription;
   publicChartsthisSub: Subscription;
   chartDetails;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afDatabase: AngularFireDatabase
+  alertOption: AlertOptions = {
+    enableBackdropDismiss: false,
+    buttons: [{ text: 'OK', role: 'cancle', handler: () => { this.navCtrl.popToRoot() } }]
+  };
+  alert
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afDatabase: AngularFireDatabase, private storage: Storage
     , private loadingCtrl: LoadingController, private authService: AuthService, private alertCtrl: AlertController) {
   }
 
   ngOnInit() {
-    let alertOption: AlertOptions = {
-      enableBackdropDismiss: false,
-      buttons: [{ text: 'OK', role: 'cancle', handler: () => { this.navCtrl.popToRoot() } }]
-    };
-    let alert
     this.loading.present();
-    setTimeout(() => {
-      let chartkey = this.navParams.get('id')
-      this.userStateSubscription = this.authService.getUserState().subscribe(
-        user => {
-          this.chartSubscription = this.afDatabase.object(`users/${user.uid}/friendsCharts/${chartkey}`)
-            .subscribe(
-            chart => {
-              if (chart.$value === undefined) {
-                this.chartDetails = chart;
-                this.loading.dismiss();
-              } else {
-                this.publicChartsthisSub = this.afDatabase.object(`publicCharts/${chartkey}`).subscribe(publicChart => {
-                  if (publicChart.$value === undefined) {
-                    this.chartDetails = publicChart;
-                    this.loading.dismiss();
-                  } else {
-                    this.userChart = this.afDatabase.object(`users/${user.uid}/userCharts/${chartkey}`).subscribe(
-                      userChart => {
-                        if (userChart.$value === undefined) {
-                          this.chartDetails = userChart;
-                          this.loading.dismiss()
-                        } else {
-                          this.loading.dismiss()
-                          alertOption.subTitle = 'you cannot see this chart, this chart is not public or it has been deleted'
-                          alert = this.alertCtrl.create(alertOption).present()
-                        }
-                      },
-                      err => {
-                        this.loading.dismiss()
-                        alertOption.subTitle = 'oops something went wrong, please try again later'
-                        alert = this.alertCtrl.create(alertOption).present()
-                      }
-                    )
-                  }
-                })
-              }
-            },
-            err => {
-              this.loading.dismiss();
-            }
-            )
-        })
-    }, 3000);
+    this.storage.get('isFirstRun').then(value => {
+      if (!value) {
+        this.loadChart()
+      } else {
+        setTimeout(() => {
+          this.loadChart()
 
+        }, 3000);
+      }
+    }).then(_=>{
+      this.storage.set('isFirstRun',false)
+    })
+  }
+  loadChart() {
+    let chartkey = this.navParams.get('id')
+    this.userStateSubscription = this.authService.getUserState().subscribe(
+      user => {
+        this.chartSubscription = this.afDatabase.object(`users/${user.uid}/friendsCharts/${chartkey}`)
+          .subscribe(
+          chart => {
+            if (chart.$value === undefined) {
+              this.chartDetails = chart;
+              this.loading.dismiss();
+            } else {
+              this.publicChartsthisSub = this.afDatabase.object(`publicCharts/${chartkey}`).subscribe(publicChart => {
+                if (publicChart.$value === undefined) {
+                  this.chartDetails = publicChart;
+                  this.loading.dismiss();
+                } else {
+                  this.userChart = this.afDatabase.object(`users/${user.uid}/userCharts/${chartkey}`).subscribe(
+                    userChart => {
+                      if (userChart.$value === undefined) {
+                        this.chartDetails = userChart;
+                        this.loading.dismiss()
+                      } else {
+                        this.loading.dismiss()
+                        this.alertOption.subTitle = 'you cannot see this chart, this chart is not public or it has been deleted'
+                        this.alert = this.alertCtrl.create(this.alertOption).present()
+                      }
+                    },
+                    err => {
+                      this.loading.dismiss()
+                      this.alertOption.subTitle = 'oops something went wrong, please try again later'
+                      this.alert = this.alertCtrl.create(this.alertOption).present()
+                    }
+                  )
+                }
+              })
+            }
+          },
+          err => {
+            this.loading.dismiss();
+          }
+          )
+      })
   }
   ngOnDestroy(): void {
     if (this.userChart)
