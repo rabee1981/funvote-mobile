c89a9c77a00f00636460c624f4c528137b98ea0b
mohamadrabee <mohamad_rabee10@yahoo.com>
2017-08-25 03:42:53 +0300

    revert to website instead of app Link

--- src/services/sharing.service.ts
+++ src/services/sharing.service.ts
@@ -1,3 +1,4 @@
+import { Http, Headers } from '@angular/http';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
@@ -10,29 +11,45 @@ import { Clipboard } from '@ionic-native/clipboard';
 export class SharingService {
   applink;
   constructor(private platform: Platform, private socialSharing: SocialSharing, private afDatabase: AngularFireDatabase,
-    private afAuth: AngularFireAuth, private clipboard: Clipboard) {
-      this.afDatabase.object('fbAppLink').take(1).subscribe(url => {
-        this.applink = url.$value
-      })
-     }
+    private afAuth: AngularFireAuth, private clipboard: Clipboard, private http: Http, private loadingCtrl: LoadingController) {
+    // this.afDatabase.object('fbAppLink').take(1).subscribe(url => {
+    //   this.applink = url.$value
+    // })
+  }
 
   share(via: ShareVia, key, base64) {
-      let shortUrl = this.applink + '?id=' + key + 'end' // url = "fb.me/344033696017700"
-      var message = shortUrl;
-      switch (via) {
-        case ShareVia.FACEBOOK: {
-          return this.facebookSharing(base64, shortUrl, message)
-        }
-        case ShareVia.INSTAGRAM: {
-          return this.instagramSharing(base64, shortUrl, message)
-        }
-        case ShareVia.TWITTER: {
-          return this.twitterSharing(base64, shortUrl, message)
-        }
-        case ShareVia.WHATSAPP: {
-          return this.whatsappSharing(base64, shortUrl, message)
-        }
-      }
+    let loading = this.loadingCtrl.create({
+      content: 'Please Wait...'
+    });
+    // let shortUrl = this.applink + '?id=' + key + 'end' // url = "fb.me/344033696017700"
+    // var message = shortUrl;
+    loading.present()
+    let headers = new Headers();
+    return this.afAuth.auth.currentUser.getIdToken().then(
+      token => {
+        headers.append('Authorization', 'Bearer ' + token)
+        this.http.get(`https://us-central1-funvaotedata.cloudfunctions.net/getShortLink?key=${key}`, { headers: headers })
+          .take(1).subscribe((res: any) => {
+            let shortUrl = res._body;
+            var message = shortUrl;
+            switch (via) {
+              case ShareVia.FACEBOOK: {
+                return this.facebookSharing(base64, shortUrl, message)
+                  .then(_ => { loading.dismiss() })
+                  .catch(_ => { loading.dismiss() })
+              }
+              case ShareVia.INSTAGRAM: {
+                return this.instagramSharing(base64, shortUrl, message)
+              }
+              case ShareVia.TWITTER: {
+                return this.twitterSharing(base64, shortUrl, message)
+              }
+              case ShareVia.WHATSAPP: {
+                return this.whatsappSharing(base64, shortUrl, message)
+              }
+            }
+          })
+      })
   }
   facebookSharing(base64, shortUrl, message) {
     this.clipboard.copy(message)
